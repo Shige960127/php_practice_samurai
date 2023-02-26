8798034e9f81575ebad6c37116c11220eb163f72
重森建人 <shige960127@shigemokerusMBP.local>
2023-02-26 21:52:31 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$dns = 'mysql:dbname=php-db-app;host=localhost;charset=utf8mb4';
+$user = 'root';
+$password = 'root';
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品管理アプリ</title>
+</head>
+<body>
+    
+</body>
+</html>
